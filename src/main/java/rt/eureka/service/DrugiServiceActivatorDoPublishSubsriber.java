1e9bdcc48cf6db5985b8ff72930a2149cea321e8
dom_linux <dom_linux@ddddd.xy>
2018-02-24 23:24:03 +0100

    publish subscriber

---
+++ src/main/java/rt/eureka/service/DrugiServiceActivatorDoPublishSubsriber.java
@@ -0,0 +1,19 @@
+package rt.eureka.service;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Service;
+
+import rt.eureka.model.WSCustomerVO;
+
+@Service
+public class DrugiServiceActivatorDoPublishSubsriber {
+
+	private static final Log log = LogFactory.getLog(DrugiServiceActivatorDoPublishSubsriber.class);
+	
+	public void processMessage(WSCustomerVO wsCustomerVO){
+		String payload = wsCustomerVO.getCountry();
+		payload = payload.concat(" alholand");
+		
+	}
+}
